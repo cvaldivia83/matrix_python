2e76d389008a24577af8178619333727ed5cd60f
Carla Valdivia <valdivia.carla@gmail.com>
2025-07-29 14:50:58 -0300

    [fix] details in get_list_column

--- project/parsing.py
+++ project/parsing.py
@@ -1,36 +1,46 @@
 import gspread
+import re
+import logging
 # this script is used with a google service account to read data from a Google Sheet
 
 
-def main():
-    gc = gspread.service_account(filename='project/credentials.json')
+def main(url: str) -> str:
+    try: 
+        gc = gspread.service_account(filename='project/credentials.json')
 
-    # open a google sheet
-    spreadsheet_id = "1q6F30Em5vanLru0VGlHP_AA02hYE9CYfn80XCcCISg8"
-    sheet = gc.open_by_key(spreadsheet_id)
-    worksheet = sheet.get_worksheet(0)
+        """ Opens a Google Spreadsheet """
+        match = re.search(r"(?<=https://docs.google.com/spreadsheets/d/)[^/]+", url)
+        if match: 
+            spreadsheet_id = match.group(0)
+        else: 
+            raise ValueError("Invalid Google Spreadsheet URL")
+        sheet = gc.open_by_key(spreadsheet_id)
+        worksheet = sheet.get_worksheet(0)
    
+        """ 1. Gets all values in the columns """
+        x_column = get_list_column(worksheet, "x-coordinate")
+        y_column = get_list_column(worksheet, "y-coordinate")
+        chars_column = get_list_column(worksheet, "Character")
 
-    # 1. Get all values in the columns and remove any empty strings
-    
-    x_column = get_list_column(worksheet, "x-coordinate")
-    y_column = get_list_column(worksheet, "y-coordinate")
-    chars_column = get_list_column(worksheet, "character")
+        """ 2. Finds the max_value(int) in the x-coordinate and y-coordinate columns """
+        max_x = find_max_value(x_column)
+        max_y = find_max_value(y_column)
 
-   
-    # # 2. Find the max_value in the x_coordinate and y_coordinate lists
-    max_x = find_max_value(x_column)
-    max_y = find_max_value(y_column)
+        """ 3. Creates an empty table structure to receive all the values. """
+        list_structure = create_table_structure(max_x, max_y)
+        
+        """ 4. Inserts the characters in the x and y position of the table. """
+        inserted_values = fill_structure(list_structure, x_column, y_column, chars_column)
 
-    # # 3. Create an empty matrix structure to receive all the values
-    list_structure = create_table_structure(max_x, max_y)
-    
-    # # 4. Insert all the values
-    inserted_values = fill_structure(list_structure, x_column, y_column, chars_column)
+        """ 5. Transforms all inner lists into a string. """
+        string_values = list(map(lambda x: "".join(x), inserted_values))
 
-    string_values = list(map(lambda x: "".join(x), inserted_values))
-
-    return "".join(string_values)
+        """ 6. Transforms inner strings into a single string. """
+        # print("".join(string_values))
+        return "".join(string_values)
+    except Exception as e: 
+        logging.error(f"{e} - Could not open Google Spreadsheet.")
+        return "Invalid Google Spreadsheet URL. Please provide a valid URL."
 
 
 def get_list_column(worksheet, title: str) -> list: 
@@ -50,10 +60,12 @@ def get_list_column(worksheet, title: str) -> list:
         all_values = worksheet.col_values(index)
         if title == "x-coordinate" or title == "y-coordinate":
             return list(map(lambda x: int(x), all_values[1:]))
+        elif title == 'Character':
+            return list(map(lambda x: x.capitalize(), all_values[1:]))
         else:
             return all_values[1:]
     except Exception as e:
-        print(f"Could not find column with title '{title}'.")
+        logging.error(f"Could not find column with title '{title}'.")
         return []
     
 
@@ -67,8 +79,7 @@ def find_max_value(column: list[int]) -> int:
         If column is empty it returns 0
     """
     if len(column) > 0:
-        integer_list = list(map(lambda x: int(x), column))
-        return max(integer_list)
+        return max(column)
     else:
         return 0
     
@@ -120,4 +131,4 @@ def fill_structure(table: list[list[str]], x: list[int], y: list[int], chars: li
         return table
 
 if __name__ == "__main__":
-    main()
+    main("https://docs.google.com/document/d/e/2PACX-1vSZ1vDD85PCR1d5QC2XwbXClC1Kuh3a4u0y3VbTvTFQI53erafhUkGot24ulET8ZRqFSzYoi3pLTGwM/pub")
